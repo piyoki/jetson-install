a95b5738441bd58058f684fa99370cfc00ec470b
yqlbu <31861128+yqlbu@users.noreply.github.com>
2020-05-25 17:34:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -233,10 +233,10 @@ rm rf nomachine_6.10.12_1_arm64.deb
 pycharm (){
 cd ~
 sudo apt-get update && sudo apt-get install -y openjdk-8-jdk
-wget https://download.jetbrains.com/python/pycharm-professional-2019.3.4.tar.gz?_ga=2.42966822.2056165753.1586158936-1955479096.1586158936 -O pycharm-professional-2019.3.4.tar.gz
-tar -xzf pycharm-professional-2019.3.4.tar.gz && cd pycharm-2019.3.4/bin
+wget https://download.jetbrains.com/python/pycharm-community-2019.3.4.tar.gz?_ga=2.42966822.2056165753.1586158936-1955479096.1586158936 -O pycharm-professional-2019.3.4.tar.gz
+tar -xzf pycharm-community-2019.3.4.tar.gz && cd pycharm-2019.3.4/bin
 sudo chmod +x pycharm.sh && mv pycharm.sh pycharm
-sudo rm -rf pycharm-professional-2019.3.4.tar.gz
+sudo rm -rf pycharm-community-2019.3.4.tar.gz
 cd ~
 echo 'export PATH=/home/'$USER'/pycharm-2019.3.4/bin:$PATH' >> .bashrc
 }
